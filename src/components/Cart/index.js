323aa7f0b43ca9e900df758eae41d8db45ee0ba2
surendra4c4 <surendra4c4@gmail.com>
2022-02-05 11:10:01 +0000

    authentication practice

---
+++ src/components/Cart/index.js
@@ -0,0 +1,18 @@
+// Write your JS code here
+import Header from '../Header'
+import './index.css'
+
+const Products = () => (
+  <>
+    <Header />
+    <div className="product-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+        alt="cart"
+        className="products-img"
+      />
+    </div>
+  </>
+)
+
+export default Products
